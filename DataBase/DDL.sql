19764df1f3ea8d534facf2d9f941c5507e99987e
javierWebDev <151965097+JavierWebDev@users.noreply.github.com>
2024-06-20 17:01:19 -0500

    feat: :sparkles: Finished DDL
    Finished the development of the airport DataBase

--- DataBase/DDL.sql
+++ DataBase/DDL.sql
@@ -8,90 +8,168 @@ USE airport_database;
 
 -- Tables creation
 
-CREATE TABLE document_types(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS document_types(
+	id INT PRIMARY KEY,
 	documenType VARCHAR(40)
 );
 
-CREATE TABLE customer(
+CREATE TABLE IF NOT EXISTS customer(
 	id VARCHAR(5) PRIMARY KEY,
 	name VARCHAR(40) NOT NULL,
+	lastName VARCHAR(40) NOT NULL,
 	age INT NOT NULL,
 	idDocumentType INT NOT NULL,
 		CONSTRAINT FK_DocumentType FOREIGN KEY (idDocumentType) REFERENCES document_types(id)
 );
 
-CREATE TABLE flight_fares(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS flight_fares(
+	id VARCHAR(5) PRIMARY KEY,
 	description VARCHAR(20) NOT NULL,
 	details TEXT,
-	value double(2,3)
+	value double
 );
 
-CREATE TABLE trip(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS trip(
+	id VARCHAR(5) PRIMARY KEY,
 	tripDate DATE NOT NULL,
-	price DOUBLE(7,3) NOT NULL
+	price DOUBLE NOT NULL
 );
 
-CREATE TABLE trip_booking(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS trip_booking(
+	id VARCHAR(5) PRIMARY KEY,
 	date DATE NOT NULL,
-	idTrip INT NOT NULL
-		CONSTRAINT FK_Trip FOREIGN KEY (idTrip) REFERENCES trip(id)
+	idTrip VARCHAR(5) NOT NULL,
+		CONSTRAINT FK_TripBooking FOREIGN KEY (idTrip) REFERENCES trip(id)
 );
 
-CREATE TABLE trip_booking_details(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	idTripBooking INT NOT NULL,
-	idCustomer VARCHAR(20) NOT NULL,
-	idFlightFares INT NOT NULL,
-		CONSTRAINT FK_TripBooking FOREIGN KEY (idTripBooking) REFERENCES trip_booking(id),
-		CONSTRAINT FK_Customer FOREIGN KEY (idCustomer) REFERENCES customer(id),
+CREATE TABLE IF NOT EXISTS trip_booking_details(
+	id VARCHAR(5) PRIMARY KEY,
+	idTripBooking VARCHAR(5) NOT NULL,
+	idCustomer VARCHAR(5) NOT NULL,
+	idFlightFares VARCHAR(5) NOT NULL,
+		CONSTRAINT FK_TripBookingDetails FOREIGN KEY (idTripBooking) REFERENCES trip_booking(id),
+		CONSTRAINT FK_CustomerBookingDetails FOREIGN KEY (idCustomer) REFERENCES customer(id),
 		CONSTRAINT FK_FlightFares FOREIGN KEY (idFlightFares) REFERENCES flight_fares(id)
 );
 
--- Employee
-
-CREATE TABLE manufacturer(
-	id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS manufacturer(
+	id VARCHAR(5) PRIMARY KEY,
 	manufacturer VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE model(
+CREATE TABLE IF NOT EXISTS model(
 	id VARCHAR(5) PRIMARY KEY,
 	model VARCHAR(50) NOT NULL,
-	idManufacturer INT NOT NULL,
+	idManufacturer VARCHAR(5) NOT NULL,
 		CONSTRAINT FK_Manufacturer FOREIGN KEY (idManufacturer) REFERENCES manufacturer(id)
 );
 
-CREATE TABLE status(
-	id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS status(
+	id VARCHAR(5) PRIMARY KEY,
 	status VARCHAR(40)
 );
 
-CREATE TABLE airline(
-	id VARCHAR(5) PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS airline(
+	id VARCHAR(5) PRIMARY KEY,
 	airline VARCHAR(40) NOT NULL
 );
 
-CREATE TABLE plane(
+CREATE TABLE IF NOT EXISTS plane(
 	id VARCHAR(5) PRIMARY KEY,
 	plates VARCHAR(30) NOT NULL,
 	capacity INT NOT NULL,
 	fabricationDate DATE NOT NULL,
 	idModel VARCHAR(5) NOT NULL,
-	idStatus INT NOT NULL,
-	idAirline
+	idStatus INT NOT NULL
 );
 
-CREATE TABLE tripulation_role(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+ CREATE TABLE IF NOT EXISTS country(
+	id VARCHAR(5) PRIMARY KEY,
+	country VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS city(
+	id VARCHAR(5) PRIMARY KEY,
+	city VARCHAR(50) NOT NULL,
+	idCountry VARCHAR(5) NOT NULL,
+		CONSTRAINT FK_ContriesCity FOREIGN KEY (idCountry) REFERENCES country(id)
+);
+
+CREATE TABLE IF NOT EXISTS airport(
+	id VARCHAR(5) PRIMARY KEY,
+	airport VARCHAR(50) NOT NULL,
+	idCity VARCHAR(5) NOT NULL,
+		CONSTRAINT FK_CityAirport FOREIGN KEY (idCity) REFERENCES city(id)
+);
+
+CREATE TABLE IF NOT EXISTS gate(
+	id VARCHAR(5) PRIMARY KEY,
+	gate VARCHAR(10) NOT NULL,
+	idAirport VARCHAR(5) NOT NULL,
+		CONSTRAINT FK_AirportGate FOREIGN KEY (idAirport) REFERENCES airport(id)
+);
+
+CREATE TABLE IF NOT EXISTS tripulation_role(
+	id VARCHAR(5) PRIMARY KEY,
 	role VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE employee(
-	
+CREATE TABLE IF NOT EXISTS employee(
+	id VARCHAR(5) PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	lastName VARCHAR(50) NOT NULL,
+	ingressDate DATE NOT NULL,
+	idRole VARCHAR(5) NOT NULL,
+	idAirline VARCHAR(5) NOT NULL,
+	idAirport VARCHAR(5) NOT NULL,
+		CONSTRAINT FK_Role FOREIGN KEY (idRole) REFERENCES tripulation_role(id),
+		CONSTRAINT FK_AirlineEmployee FOREIGN KEY (idAirline) REFERENCES airline(id),
+		CONSTRAINT FK_AirportEmployee FOREIGN KEY (idAirport) REFERENCES airport(id)
 );
+
+CREATE TABLE IF NOT EXISTS flight_connection(
+	id VARCHAR(5) PRIMARY KEY,
+	conectionOrder INT NOT NULL,
+	idTrip VARCHAR(5) NOT NULL,
+	idPlane VARCHAR(5) NOT NULL,
+	idAirportA VARCHAR(5) NOT NULL,
+	idAirportB VARCHAR(5) NOT NULL,
+		CONSTRAINT FK_TripConnection FOREIGN KEY (idTrip) REFERENCES trip(id),
+		CONSTRAINT FK_PlaneConnection FOREIGN KEY (idPlane) REFERENCES plane(id),
+		CONSTRAINT FK_AirportA FOREIGN KEY (idAirportA) REFERENCES airport(id),
+		CONSTRAINT FK_TripAirportB FOREIGN KEY (idAirportB) REFERENCES airport(id)
+);
+
+CREATE TABLE IF NOT EXISTS trip_crews(
+	idEmployee VARCHAR(5),
+	idConnection VARCHAR(5),
+		CONSTRAINT FK_EmployeeTrip FOREIGN KEY (idEmployee) REFERENCES employee(id),
+		CONSTRAINT FK_Connection FOREIGN KEY (idConnection) REFERENCES flight_connection(id)
+);
+
+CREATE TABLE IF NOT EXISTS revision_details(
+	id VARCHAR(5) PRIMARY KEY,
+	description TEXT,
+	idEmployee VARCHAR(5) NOT NULL,
+		CONSTRAINT FK_EmployeeRevDetails FOREIGN KEY (idEmployee) REFERENCES employee(id)
+);
+
+CREATE TABLE IF NOT EXISTS revision(
+	id VARCHAR(5) PRIMARY KEY,
+	revisionDate DATE NOT NULL,
+	idPlane VARCHAR(5) NOT NULL,
+	idDetails VARCHAR(5) NOT NULL,
+		CONSTRAINT FK_PlaneRevision FOREIGN KEY (idPlane) REFERENCES plane(id),
+		CONSTRAINT FK_RevDetails FOREIGN KEY (idDetails) REFERENCES revision_details(id)
+);
+
+CREATE TABLE IF NOT EXISTS revision_employee(
+	idEmployee VARCHAR(5) NOT NULL,
+	idRevision VARCHAR(5) NOT NULL,
+		CONSTRAINT FK_EmployeeRevision FOREIGN KEY (idEmployee) REFERENCES employee(id),
+		CONSTRAINT FK_RevisionRevision FOREIGN KEY (idRevision) REFERENCES revision(id)
+);
+
+
 
 
