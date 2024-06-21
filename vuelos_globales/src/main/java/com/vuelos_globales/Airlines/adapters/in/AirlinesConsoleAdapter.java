95f19b35c4b51285a8fd3219489370c3c558862e
dvn495 <152308538+dvn495@users.noreply.github.com>
2024-06-21 09:07:56 -0500

    feat: :construction: Begin of Airlines Adapters In
    Begin the elaboration of the Console adpater in Airlines

--- vuelos_globales/src/main/java/com/vuelos_globales/Airlines/adapters/in/AirlinesConsoleAdapter.java
+++ vuelos_globales/src/main/java/com/vuelos_globales/Airlines/adapters/in/AirlinesConsoleAdapter.java
@@ -1,5 +1,15 @@
 package com.vuelos_globales.Airlines.adapters.in;
 
 public class AirlinesConsoleAdapter {
-    
+    private final AirlinesService airlineService;
+
+    public PaisConsoleAdapter(AirlinesService airlineService) {
+        this.airlineService = airlineService;
+    }
+
+    public void start(){
+        Scanner scanner = new Scanner(System.in);
+
+        while(true)
+    }
 }
