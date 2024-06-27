c5ad9b4a568e35da44b5f416b2b4ac2ed76cd896
Nakrz <151976098+Nakrz@users.noreply.github.com>
2024-06-27 06:43:15 -0500

    feat: :sparkles: New use cases

--- vuelos_globales/src/main/java/com/vuelos_globales/entities/Customer/adapters/in/CustomerConsoleAdapter.java
+++ vuelos_globales/src/main/java/com/vuelos_globales/entities/Customer/adapters/in/CustomerConsoleAdapter.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import com.vuelos_globales.entities.Customer.application.CustomerService;
 import com.vuelos_globales.entities.Customer.domain.Customer;
+import com.vuelos_globales.entities.DocumentType.domain.DocumentType;
 import com.vuelos_globales.modules.ConsoleUtils;
 
 
@@ -20,48 +21,87 @@ public class CustomerConsoleAdapter {
     public CustomerConsoleAdapter(CustomerService customerService) {
         this.customerService = customerService;
     }
-
     public void createCustomer() {
         String rta = "S";
-
+    
         while (rta.equalsIgnoreCase("S")) {
             ConsoleUtils.limpiarConsola();
             System.out.println("*************** REGISTRAR CLIENTE ***************");
             System.out.println("[*] INGRESE EL ID DEL CLIENTE A CREAR: ");
             String id = sc.nextLine();
+    
+            
             Optional<Customer> customer = customerService.getCustomerById(id);
-            customer.ifPresentOrElse(
-                c -> {
-                    System.out.println(MessageFormat.format("[!] EL ID (0) YA ESTA OCUPADO.", c.getId()));
-                },
-                () -> {
-                    ConsoleUtils.limpiarConsola();
-                    System.out.println("*************** REGISTRAR CLIENTE ***************");
-
-                    System.out.println("[*] INGRESE EL NOMBRE DEL CLIENTE: ");
-                    String customerName = sc.nextLine();
+            if (customer.isPresent()) {
+                System.out.println(MessageFormat.format("[!] EL ID ({0}) YA ESTA OCUPADO.", id));
+                ConsoleUtils.esperarEntrada();
+                continue; 
+            }
     
-                    System.out.println("[*] INGRESE EL APELLIDO DEL CLIENTE: ");
-                    String customerLastName = sc.nextLine();
+            ConsoleUtils.limpiarConsola();
+            System.out.println("*************** REGISTRAR CLIENTE ***************");
+            System.out.println("[*] INGRESE EL NOMBRE DEL CLIENTE: ");
+            String customerName = sc.nextLine();
     
+            System.out.println("[*] INGRESE EL APELLIDO DEL CLIENTE: ");
+            String customerLastName = sc.nextLine();
+    
+            int customerAge = 0;
+            boolean validAge = false;
+            while (!validAge) {
+                try {
                     System.out.println("[*] INGRESE LA EDAD DEL CLIENTE: ");
-                    int customerAge = sc.nextInt();
+                    customerAge = Integer.parseInt(sc.nextLine().trim());
+                    validAge = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("[!] INGRESASTE UNA OPCION INVALIDA PARA EDAD DEL CLIENTE.");
+                }
+            }
     
-                    System.out.println("[*] INGRESE EL TIPO DE DOCUMENTO DEL CLIENTE: ");
-                    int customerDocType = sc.nextInt();
+            int idDocument = 0;
+            boolean documentTypeCreated = false;
     
-                    Customer newCustomer = new Customer(id, customerName, customerLastName, customerAge, customerDocType);
-                    customerService.createCustomer(newCustomer);
-                });
-
+            while (!documentTypeCreated) {
+                ConsoleUtils.limpiarConsola();
+                try {
+                    System.out.println("*************** REGISTRAR TIPO DE DOCUMENTO ***************");
+                    System.out.println("[*] INGRESE EL ID DEL TIPO DE DOCUMENTO A CREAR: ");
+                    idDocument = Integer.parseInt(sc.nextLine().trim());
+    
+                    Optional<DocumentType> documentType = customerService.getDocumentTypeById(idDocument);
+                    if (documentType.isPresent()) {
+                        System.out.println(MessageFormat.format("[!] EL ID ({0}) YA ESTA OCUPADO.", idDocument));
+                        ConsoleUtils.esperarEntrada();
+                        continue;
+                    }
+    
+                    ConsoleUtils.limpiarConsola();
+                    System.out.println("*************** REGISTRAR TIPO DE DOCUMENTO ***************");
+                    System.out.println("[*] INGRESE EL NOMBRE DEL TIPO DE DOCUMENTO: ");
+                    String docTypeName = sc.nextLine();
+    
+                    DocumentType newDocumentType = new DocumentType(idDocument, docTypeName);
+                    customerService.createDocumentType(newDocumentType);
+                    documentTypeCreated = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("[!] INGRESASTE UNA OPCION INVALIDA PARA ID DE DOCUMENTO.");
+                    ConsoleUtils.esperarEntrada();
+                }
+            }
+    
+            Customer newCustomer = new Customer(id, customerName, customerLastName, customerAge, idDocument);
+            customerService.createCustomer(newCustomer);
+    
             System.out.println("[?] DESEA AÑADIR OTRO CLIENTE? [S] - SI | [INGRESE CUALQUIER TECLA] - NO");
             rta = sc.nextLine();
         }
     }
+    
+
 
     public void searchCustomer() {
         List<Customer> customers = customerService.getAllCustomers();
-        
+            
         if (customers.isEmpty()) {
             ConsoleUtils.limpiarConsola();
             System.out.println("[!] NO HAY NINGUN CLIENTE REGISTRADO");
@@ -71,29 +111,32 @@ public class CustomerConsoleAdapter {
             System.out.println("*************** BUSCAR CLIENTE ***************");
             System.out.println("[?] INGRESE EL ID DEL CLIENTE A BUSCAR: ");
             String findId = sc.nextLine();
-
+    
             Optional<Customer> customer = customerService.getCustomerById(findId);
             customer.ifPresentOrElse(
                 c -> {
                     ConsoleUtils.limpiarConsola();
                     System.out.println("*************** CLIENTE ***************");
-                    System.out.println(MessageFormat.format("[*] ID : {0}\n[*] NOMBRE : {1}\n[*] EDAD: {2}\n[*] DOCUMENTO: {3}", c.getId(), c.getName() + c.getLastName(), c.getIdDocumentType()));
-                    sc.nextLine();
+                    
+                    int idDocumentType = c.getIdDocumentType();
+                    System.out.println(MessageFormat.format("[*] ID : {0}\n[*] NOMBRE : {1}\n[*]APELLIDO : {2}\n[*] EDAD: {3}\n[*] DOCUMENTO ID: {4}", c.getId(), c.getName(), c.getLastName(), c.getAge(), idDocumentType));
+                    ConsoleUtils.esperarEntrada();
                 },
                 () -> {
                     ConsoleUtils.limpiarConsola();
                     System.out.println("[!]  CLIENTE NO ENCONTRADO");
-                    sc.nextLine();
+                    ConsoleUtils.esperarEntrada();
                 });
                 ConsoleUtils.limpiarConsola();
                 System.out.println("[*]  PRESIONE CUALQUIER TECLA PARA CONTINUAR...");
                 sc.nextLine();
         }
     }
+    
 
     public void updateCustomer() {
         List<Customer> customers = customerService.getAllCustomers();
-
+    
         if (customers.isEmpty()) {
             ConsoleUtils.limpiarConsola();
             System.out.println("[!] NO HAY NINGUN CLIENTE REGISTRADO");
@@ -102,44 +145,60 @@ public class CustomerConsoleAdapter {
             ConsoleUtils.limpiarConsola();
             System.out.println("[?] INGRESE EL ID DEL CLIENTE A BUSCAR: ");
             String findId = sc.nextLine();
-
+    
             Optional<Customer> customer = customerService.getCustomerById(findId);
             customer.ifPresentOrElse(
-            c -> {
-                ConsoleUtils.limpiarConsola();
-                System.out.println("*************** ACTUALIZAR CLIENTE ***************");
-                System.out.println(MessageFormat.format("[*] ID : {0}\n[*] NOMBRE : {1}\n[*] EDAD: {2}\n[*] DOCUMENTO: {3}", c.getId(), c.getName() + c.getLastName(), c.getIdDocumentType()));
-
-                System.out.println("[?] INGRESE EL NOMBRE DEL CLIENTE : ");
-                String updateName = sc.nextLine();
-
-                System.out.println("[?] INGRESE EL APELLIDO DEL CLIENTE : ");
-                String updateLastName = sc.nextLine();
-
-                System.out.println("[?] INGRESE LA EDAD DEL CLIENTE : ");
-                int updateAge = sc.nextInt();
-
-                System.out.println("[?] INGRESE TIPO DE DOCUMENTO DEL CLIENTE : ");
-                int updateCostumerClient = sc.nextInt();
-            
-                Customer updatedCustomer = new Customer(c.getId(), updateName, updateLastName, updateAge, updateCostumerClient);
-                customerService.updateCustomer(updatedCustomer);
-                sc.nextLine();
-
-                ConsoleUtils.limpiarConsola();
-                System.out.println("[*] CLIENTE ACTUALIZADO CORRECTAMENTE.");
-                sc.nextLine();
-            },
-            () -> {
-                ConsoleUtils.limpiarConsola();
-                System.out.println("[!] CLIENTE NO ENCONTRADO");
-                sc.nextLine();
-            }
+                c -> {
+                    ConsoleUtils.limpiarConsola();
+                    System.out.println("*************** ACTUALIZAR CLIENTE ***************");
+                    System.out.println(MessageFormat.format("[*] ID : {0}\n[*] NOMBRE : {1}\n[*]APELLIDO : {2}\n[*] EDAD: {3}\n[*] DOCUMENTO ID: {4}", c.getId(), c.getName(), c.getLastName(), c.getAge(), c.getIdDocumentType()));
+    
+                    System.out.println("[?] INGRESE EL NUEVO NOMBRE DEL CLIENTE: ");
+                    String updateName = sc.nextLine();
+    
+                    System.out.println("[?] INGRESE EL NUEVO APELLIDO DEL CLIENTE: ");
+                    String updateLastName = sc.nextLine();
+    
+                    int updateAge = 0;
+                    boolean validAge = false;
+                    while (!validAge) {
+                        try {
+                            System.out.println("[?] INGRESE LA NUEVA EDAD DEL CLIENTE: ");
+                            updateAge = Integer.parseInt(sc.nextLine().trim());
+                            validAge = true;
+                        } catch (NumberFormatException e) {
+                            System.out.println("[!] INGRESASTE UNA OPCION INVALIDA PARA EDAD DEL CLIENTE.");
+                        }
+                    }
+    
+                    int updateCustomerClient = 0;
+                    boolean documentType = false;
+                    while (!documentType) {
+                        try {
+                            System.out.println("[?] INGRESE EL NUEVO TIPO DE DOCUMENTO DEL CLIENTE: ");
+                            updateCustomerClient = Integer.parseInt(sc.nextLine().trim());
+                            documentType = true;
+                        } catch (NumberFormatException e) {
+                            System.out.println("[!] INGRESASTE UNA OPCION INVALIDA PARA TIPO DE DOCUMENTO.");
+                        }
+                    }
+                    Customer updatedCustomer = new Customer(c.getId(), updateName, updateLastName, updateAge, updateCustomerClient);
+                    customerService.updateCustomer(updatedCustomer);
+                    ConsoleUtils.limpiarConsola();
+                    System.out.println("[*] CLIENTE ACTUALIZADO CORRECTAMENTE.");
+                    sc.nextLine(); 
+                },
+                () -> {
+                    ConsoleUtils.limpiarConsola();
+                    System.out.println("[!] CLIENTE NO ENCONTRADO");
+                    sc.nextLine(); 
+                }
             );
             System.out.println("[*]  PRESIONE CUALQUIER TECLA PARA CONTINUAR...");
-            sc.nextLine();
+            sc.nextLine(); 
         }
     }
+    
 
     public void deleteCustomer() {
         List<Customer> customers = customerService.getAllCustomers();
